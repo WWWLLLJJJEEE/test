e1c8c127730926cbdefbb4a646205fc42bd96ace
WWWLLLJJJEEE <dnwpwpdhkd@naver.com>
2017-12-11 21:31:26 +0900

    []일단 함..

--- UML01Board/src/main/java/com/koitt/board/controller/UserRestController.java
+++ UML01Board/src/main/java/com/koitt/board/controller/UserRestController.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -141,7 +140,7 @@ public class UserRestController {
 		UserInfo item = null;
 		if (id != null) {
 			item = userInfoService.detail(id);
-				
+			
 			if (item != null) {
 				HttpHeaders headers = new HttpHeaders();
 				headers.setLocation(ucBuilder.path(UPLOAD_FOLDER + "/{avatar}")
@@ -150,7 +149,7 @@ public class UserRestController {
 				return new ResponseEntity<UserInfo>(item, headers, HttpStatus.OK);
 			}
 		}
-		
+
 		return new ResponseEntity<UserInfo>(new UserInfo(), HttpStatus.NO_CONTENT);
 	}
 }
